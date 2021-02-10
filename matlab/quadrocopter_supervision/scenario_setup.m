a911cdf536b394ba83eaa7cfb4867e3a4c0c35e9
Luigi Pannocchi <l.pannocchi@gmail.com>
2021-02-10 14:05:48 -0800

    Tuning the disturbance; Splittet the script into computation/plotting

--- matlab/quadrocopter_supervision/scenario_setup.m
+++ matlab/quadrocopter_supervision/scenario_setup.m
@@ -31,7 +31,13 @@ DSets = cell(length(Sp),1);
 k = mdl_dt.Nx;
 Ed = eye(k);
 Gw = [eye(k); -eye(k)];
-Fw = 1e-2 * [ones(k,1); ones(k,1)];
+
+Fw_pos = 1e-2 * ones(3,1);
+Fw_vel = 1e-2 * ones(3,1);
+Fw_acc = 5e-2 * ones(3,1);
+Fw_pva = [Fw_pos; Fw_vel; Fw_acc];
+
+Fw = [Fw_pva; Fw_pva];
 W = Polyhedron('H', [Gw Fw]);
 parfor i = 1:length(Sp)
     safeA = [G_state; [Sp(i).A zeros(size(Sp(i).A,1),6)]];
@@ -105,40 +111,5 @@ Plant_dt = ss(mdl_dt.Ad_1d, mdl_dt.Bd_1d, eye(3), []);
 %% Initialize the Simulink environment:
 InitSimulink;
 
-%% Plot Simulink output:
-figure;
-sim_output = out;
+export_data;
 
-% Plot corrected trajectory:
-plot3(sim_output.X_sim(:, 1), sim_output.X_sim(:, 2), sim_output.X_sim(:, 3),'LineWidth',3.5);
-hold on;
-grid on;
-axis equal;
-xlabel("x");
-ylabel("y");
-zlabel("z");
-
-% Plot obstacles and start/finish points:
-for i = 1:length(Obs)
-    plot(Obs(i),'color',[0.6350 0.0780 0.1840],'alpha',0.3);
-    cisA = Obs(i).chebyCenter();
-    plot3(cisA.x(1),cisA.x(2),cisA.x(3),'d','Color','red','LineWidth',5);
-end
-plot3(x0(1),x0(2),x0(3),'s','Color','magenta','LineWidth',2.5);
-text(x0(1),x0(2),x0(3),'\leftarrow start','FontSize',14);
-plot3(xT(1),xT(2),xT(3),'o','Color','green','LineWidth',2.5);
-text(xT(1),xT(2),xT(3),'\leftarrow finish','FontSize',14);
-
-xlim([-3,3]);
-ylim([-3,3]);
-
-% Plot nominal trajectory:
-Nsim = Tend/dt;             % simulation steps
-trj_pos = [];
-for step = 1 : Nsim
-    % Get trajectory:
-    trj_pos = [trj_pos; (trajectory.trj_eval(step * dt, 0))'];
-end
-plot3(trj_pos(:, 1), trj_pos(:, 2), trj_pos(:, 3),'Color', [1 0.8 0], 'LineWidth',3.5);
-
-hold off;
